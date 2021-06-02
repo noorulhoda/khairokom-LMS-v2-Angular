686954fa1c81142571103483845ec28681f3da1a
noor ulhoda <noooralhoda2030@gmail.com>
2021-06-03 01:08:32 +0200

    user auth update

--- src/app/users/get-all/get-all.component.ts
+++ src/app/users/get-all/get-all.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RolesService } from 'src/app/services/roles.service';
+import { Irole } from 'src/app/shared/Irole';
 import { UsersService } from '../../services/users.service';
 import{Iuser} from '../../shared/Iuser'
 @Component({
@@ -8,14 +10,20 @@ import{Iuser} from '../../shared/Iuser'
 })
 export class GetAllComponent implements OnInit {
  userList:Iuser[];
+ rolesList:Irole[];
  errMsg='';
-  constructor(private userservice:UsersService) {
+  constructor(private userservice:UsersService,private roleService:RolesService) {
      
   }
 neededRole:string;
   ngOnInit(): void {
   this.userservice.GetAllusers().subscribe(
       data => this.userList= data,
+      er =>this.errMsg=er ,
+    )
+
+    this.roleService.GetAllroles().subscribe(
+      data => this.rolesList= data,
       er =>this.errMsg=er ,
     )
     

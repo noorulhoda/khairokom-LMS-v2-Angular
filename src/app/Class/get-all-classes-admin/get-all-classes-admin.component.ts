36baf1bb07bc8adff9de0e039b40b82a94c21549
Nawal Zaki <nawalzaki333@gmail.com>
2021-06-19 05:19:35 +0200

    update Delete Class

--- src/app/Class/get-all-classes-admin/get-all-classes-admin.component.ts
+++ src/app/Class/get-all-classes-admin/get-all-classes-admin.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { classService } from 'src/app/services/class.service';
+import { SessionService } from 'src/app/services/session.service';
 import { Iclass } from 'src/app/shared/Iclass';
 import { Icourse } from 'src/app/shared/Icourse';
+import { Isession } from 'src/app/shared/Isession';
 
 @Component({
   selector: 'app-get-all-classes-admin',
@@ -14,8 +16,10 @@ export class GetAllClassesAdminComponent implements OnInit {
   course:Icourse;
   id:string='defaultID';
   errMsg='errroor';
+  sessions:Isession[]=[];
   classes: Iclass[] = [];
-  constructor(private classServices: classService,private router:Router,private route:ActivatedRoute) {
+  classSessions=[];
+  constructor(private classServices: classService,private sessionService:SessionService,private router:Router,private route:ActivatedRoute) {
     this.getClasses();
   }
 
@@ -29,16 +33,50 @@ export class GetAllClassesAdminComponent implements OnInit {
       }
     );
   }
+ 
+  sureDelete:Boolean=false;
+  deleteNew:Boolean=true;
   delete(id) {
-    this.classServices.deleteCLass(id)
-      .subscribe(
+    if(this.deleteNew){
+      alert(" سوف تقوم بحذف المجموعة و كذلك الحلقات التى تحتوى عليها هذه المجموعة إذا كنت متأكدا أغلق هذه النافذة واضغط مرة أخرى  على زر الحذف ")
+      this.sureDelete=true; 
+      this.deleteNew=false;
+     }
+     else if(this.sureDelete){
+
+    this.classServices.deleteCLass(id).subscribe(
         data => {
-          this.router.navigateByUrl("")
+          this.sessionService.GetAllSessions().subscribe (
+            data=>{
+              this.sessions=data
+              console.log(this.sessions);
+            
+              this.sessions.forEach(element => {
+                if(element.classId==id)
+                {
+                  this.classSessions.push(element);
+                }
+              });
+              console.log(this.classSessions)
+              
+               this.classSessions.forEach(element=>{
+                  this.sessionService.DeleteSession(element._id).subscribe(
+                    data=>{console.log(data)},
+                    error=>{console.log(error)}
+
+                  )
+            })},
+            er=>{
+              console.log(er);
+            }
+          )
         },
         error => {
           console.log("Error-_-" + error)
         }
       );
+      window.location.reload();
+  }
   }
 
 }

9447c0f9238a05d6e139b4969b57a698b4bed612
YOmnAA98 <58415363+YOmnAA98@users.noreply.github.com>
2021-06-25 18:19:12 +0200

    change service

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
-export const baseUrl='http://localhost:8050'
+export const baseUrl='https://khairokomapi.azurewebsites.net'
 //'https://khairokom2.azurewebsites.net'
 
 //'https://noon1.azurewebsites.net/'

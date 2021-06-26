25defded83bf9ccd354fc3ff3c89bc6726082173
YOmnAA98 <58415363+YOmnAA98@users.noreply.github.com>
2021-06-26 21:17:01 +0200

    update

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
-export const baseUrl='https://khairokomapi.azurewebsites.net'
+export const baseUrl='https://khairokomapi1.azurewebsites.net'
 //'https://khairokom2.azurewebsites.net'
 
 //'https://noon1.azurewebsites.net/'

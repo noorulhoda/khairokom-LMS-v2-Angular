42468adf2421276b2fd4a274e3fbccb7b3ba539f
noorulhoda <noooralhoda2030@gmail.com>
2021-09-18 16:17:15 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import{NotFoundComponent} from './not-found/not-found.component';
 
+
 import{HomeComponent} from './home/home.component';
 
 import {AddCateogryComponent} from './Category/add-cateogry/add-cateogry.component';
